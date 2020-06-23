0fe97d3f23c63039034f8f235d1b2531b0279afe
EC2 Default User <ec2-user@ip-172-31-30-136.us-east-2.compute.internal>
2020-06-23 08:34:59 +0000

    ユーザー認証の実装

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('develop/create', 'Admin\DevelopController@add')->middleware('auth');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
